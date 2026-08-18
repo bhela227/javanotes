beadcd232e76e58a31ce8f648ba25cd1d336046a
bhela227 <bhela227@gmail.com>
2026-08-18 00:46:15 -0700

    commit - dev2

--- src/car/Alto.java
+++ src/car/Alto.java
@@ -21,7 +21,7 @@ public class Alto implements Vehicle{
 	
 	public void myCar() {
 
-		System.out.println("This is added my remote");
+		System.out.println("This is added by remote");
 		System.out.println("this is added by home-to check");
 		
 	}
